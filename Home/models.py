79777024e8aa77e9312af77f05db2154a4b4b925
Apoorv Garg <u.garg.10@gmail.com>
2020-07-03 21:04:31 +0530

    Make Dynamic

--- Home/models.py
+++ Home/models.py
@@ -6,45 +6,70 @@ class About(models.Model):
     image=models.ImageField(upload_to="images")
     logo=models.ImageField(upload_to="images")
     favicon=models.ImageField(upload_to="images")
-    name=models.CharField(max_length=30)
+    first_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
     job=models.CharField(max_length=100)
     introduction=models.TextField()
     link=models.CharField(max_length=300)
     all_work=models.CharField(max_length=300)
     all_podcast=models.CharField(max_length=300)
 
+    def __str__(self):
+        return self.first_name
+
 class Work(models.Model):
     image=models.ImageField(upload_to="images")
     name=models.CharField(max_length=30)
     link=models.CharField(max_length=300)
+    
+    def __str__(self):
+        return self.name
 
 class Paper(models.Model):
     name=models.CharField(max_length=30)
     first_word=models.CharField(max_length=10)
     abstract=models.TextField()
     link=models.CharField(max_length=300)
+    
+    def __str__(self):
+        return self.name
 
 class Count(models.Model):
     name=models.CharField(max_length=30)
     number=models.IntegerField()
     icon=models.ImageField(upload_to="images")
+    
+    def __str__(self):
+        return self.name
 
 class Podcast(models.Model):
     image=models.ImageField(upload_to="images")
     name=models.CharField(max_length=30)
     link=models.CharField(max_length=300)
+    
+    def __str__(self):
+        return self.name
 
 class Skill(models.Model):
     name=models.ImageField(upload_to="images")
     percentage=models.IntegerField()
     color=models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
     name=models.CharField(max_length=50)
     abstract=models.TextField()
     link=models.CharField(max_length=300)
+    
+    def __str__(self):
+        return self.name
 
 class Link(models.Model):
     name=models.CharField(max_length=50)
     icon=models.CharField(max_length=50)
     link=models.CharField(max_length=300)
+    
+    def __str__(self):
+        return self.name
--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request,"index.html")
+    about=About.objects.all()[0]
+    work=Work.objects.all()
+    paper=Paper.objects.all()
+    count=Count.objects.all()
+    podcast=Podcast.objects.all()
+    skill=Skill.objects.all()
+    article=Article.objects.all()
+    link=Link.objects.all()
+
+    context={
+        'about':about,
+        'works':work,
+        'papers':paper,
+        'counter':count,
+        'podcasts':podcast,
+        'skills':skill,
+        'articles':article,
+        'links':link,
+    }
+
+    return render(request,"index.html",context)
