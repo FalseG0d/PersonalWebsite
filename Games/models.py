5097de74ac10566d870310007c61a80b3338c9d3
Apoorv Garg <u.garg.10@gmail.com>
2020-09-26 14:53:28 +0530

    Commit

---
+++ Games/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+'''
+class UnityGame(models.Model):
+    name=models.CharField(max_length=20)
+    play_link=models.URLField(max_length=200,default='#')
+    itch_link=models.URLField(max_length=200,default='#')
+    image=models.ImageField(upload_to="gameimages")
+    description=models.TextField()
+    def __str__(self):
+        return self.name
+'''
--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render,redirect
 from .models import *
+import smtplib
 from .forms import MessageForm
+
+
+#Email And Password
+
+host_email='codingprac10@gmail.com'
+host_password="3',>tKPdc/#2#qU!"
 
 # Create your views here.
 
@@ -35,6 +42,22 @@ def mail(request):
     if request.method=='POST':
         form=MessageForm(request.POST)
         if form.is_valid:
-            form.save()
+            sender_name=request.POST['name']
+            sender_email=request.POST['email']
+            sender_subject=request.POST['subject']
+            sender_message=request.POST['message']
+
+            with smtplib.SMTP('smtp.gmail.com',587) as smtp:
+                smtp.ehlo()
+                smtp.starttls()
+                smtp.ehlo()
+                
+                #Email must allow Secondary Applications
+                smtp.login(host_email,host_password)
+                sub="Message from Personal Website: "+sender_subject
+                body="Hi Apoorv Garg,\nA person who is called "+sender_name+", just visited your website and left the following Message. His name, details and other information are as follows, please get back to them As soon as possible:\nName:\t"+sender_name+"\nEmail:\t"+sender_email+"\nSubject:\t"+sender_subject+"\nmessage:\t"+sender_message
+                to="garg.apoorv1098@gmail.com"
+                msg=f'Subject:{sub}\n\n{body}'
+                smtp.sendmail('email',to,msg)
     
     return redirect('/')
